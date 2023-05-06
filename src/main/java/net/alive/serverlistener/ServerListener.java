878ef0c0e8c8d3ab31d4f39898ff34dfaf2dcc2d
Alive <nicolasfr002@gmail.com>
2023-05-06 13:19:21 +0200

    Initial commit

---
+++ src/main/java/net/alive/serverlistener/ServerListener.java
@@ -0,0 +1,14 @@
+package net.alive.serverlistener;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerListener implements ModInitializer {
+	public static final String MOD_ID = "serverlistener";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/alive/serverlistener/ServerListenerClient.java
@@ -0,0 +1,10 @@
+package net.alive.serverlistener;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ServerListenerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
